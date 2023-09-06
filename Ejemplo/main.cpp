b4b7b98a83b04bf71f8321643a653b53ce17cdac
Alfredo Villagran Olguin <avillagran@ipn.mx>
2023-09-06 12:36:22 -0600

    Clases Punto, Complejo y Fraccion

--- Ejemplo/main.cpp
+++ Ejemplo/main.cpp
@@ -6,57 +6,54 @@ class Punto{
 public:
     double x;
     double y;
-    void pideTusDatos(void);
-    void muestraTusDatos(void);
 };
-void Punto::pideTusDatos(void){
-    cout<<"Dame mi x ";cin>>x;
-    cout<<"Dame mi y ";cin>>y;
-}
-void Punto::muestraTusDatos(void){
-    cout<<"("<<x<<", "<<y<<")"<<endl;
-}
+
+class Complejo{
+public:
+    double real;
+    double imaginario;
+};
+
+class Fraccion{
+public:
+    int numerador;
+    int denominador;
+};
 
 int main(void){
-    Punto A;//Se construye objeto tipo Punto
-    Punto B;//Se construye objeto tipo Punto
-    Punto C;//Se construye objeto tipo Punto
+    Punto P;    //Se construye objeto tipo Punto
+    Complejo C; //Se construye objeto tipo Complejo
+    Fraccion F; //Se construye objeto tipo Fraccion
 
-    cout << "A ocupa\t  "<< sizeof(A) << " bytes" << endl;//Memoria reservada para objeto
-    cout << "B ocupa\t  "<< sizeof(B) << " bytes" << endl;//Memoria reservada para objeto
+    cout << "P ocupa\t  "<< sizeof(P) << " bytes" << endl;//Memoria reservada para objeto
     cout << "C ocupa\t  "<< sizeof(C) << " bytes" << endl;//Memoria reservada para objeto
+    cout << "F ocupa\t  "<< sizeof(F) << " bytes" << endl;//Memoria reservada para objeto
     cout<<endl<<endl;
     system("pause");
     system("cls");
 
 
-    cout<<"A";
-    A.muestraTusDatos();//Se muestran los atributos del objeto (con datos basura)
+    cout << "P = ("<<P.x<<", "<<P.y<<")"<< endl;//Se muestran los atributos del objeto (con datos basura)
     cout<<endl<<endl;
-    cout<<"B";
-    B.muestraTusDatos();//Se muestran los atributos del objeto (con datos basura)
+    cout << "C = "<<C.real<<" + "<<C.imaginario<<"i"<<endl;//Se muestran los atributos del objeto (con datos basura)
     cout<<endl<<endl;
-    cout<<"C";
-    C.muestraTusDatos();//Se muestran los atributos del objeto (con datos basura)
+    cout << "F = "<< F.numerador<<" / "<<F.denominador<<endl;//Se muestran los atributos del objeto (con datos basura)
     cout<<endl<<endl;
     system("pause");
     cout<<endl<<endl;
 
-    A.x = 1.6;      //Asignacion de una dato a atributo de objeto
-    A.y = 2.8;      //Asignacion de una dato a atributo de objeto
-    B.x = 3.6;      //Asignacion de una dato a atributo de objeto
-    B.y = 4.8;      //Asignacion de una dato a atributo de objeto
-    C.x = 5.6;      //Asignacion de una dato a atributo de objeto
-    C.y = 6.8;      //Asignacion de una dato a atributo de objeto
+    P.x = 1.2;          //Asignacion de una dato a atributo de objeto
+    P.y = 3.4;          //Asignacion de una dato a atributo de objeto
+    C.real = 5.6;       //Asignacion de una dato a atributo de objeto
+    C.imaginario = 7.8; //Asignacion de una dato a atributo de objeto
+    F.numerador = 9;    //Asignacion de una dato a atributo de objeto
+    F.denominador = 10; //Asignacion de una dato a atributo de objeto
 
-    cout<<"A";
-    A.muestraTusDatos();//Se muestran los atributos del objeto (ya no hay basura)
+    cout << "P = ("<<P.x<<", "<<P.y<<")"<< endl;//Se muestran los atributos del objeto (ya no hay basura)
     cout<<endl<<endl;
-    cout<<"B";
-    B.muestraTusDatos();//Se muestran los atributos del objeto (ya no hay basura)
+    cout << "C = "<<C.real<<" + "<<C.imaginario<<"i"<<endl;//Se muestran los atributos del objeto (ya no hay basura)
     cout<<endl<<endl;
-    cout<<"C";
-    C.muestraTusDatos();//Se muestran los atributos del objeto (ya no hay basura)
+    cout << "F = "<< F.numerador<<" / "<<F.denominador<<endl;//Se muestran los atributos del objeto (ya no hay basura)
     cout<<endl<<endl;
 
     return 0;
