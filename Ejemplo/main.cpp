43852cd965e7846fd60bdd38c003b531a35e58eb
Alfredo Villagran Olguin <avillagran@ipn.mx>
2023-09-16 06:35:51 -0600

    Nodos inicializados con valor y deireccion de otro Nodo

--- Ejemplo/main.cpp
+++ Ejemplo/main.cpp
@@ -11,6 +11,11 @@ public:
         inferior = NULL;
         cout<<"Nodo construido..."<<endl;
     };
+    Nodo(int d, Nodo* i){
+        dato = d;
+        inferior = i;
+        cout<<"Nodo construido..."<<endl;
+    };
     ~Nodo(void){
         cout<<"Nodo destruido..."<<endl;
     };
@@ -19,22 +24,29 @@ public:
 int main(void){
     cout<<"Inicio del main"<<endl<<endl;
 
-    Nodo A, B, C, D;    //Se construyen Nodos inicializados
+    Nodo D(3, NULL);    //Se construye Nodo inicializado
+    Nodo C(5, &D);      //Se construye Nodo inicializado
+    Nodo B(7, &C);      //Se construye Nodo inicializado
+    Nodo A(-4, &B);     //Se construye Nodo inicializado
     system("pause");
     system("cls");
 
-    cout<<"Nodos con cero y NULL:"<<endl<<endl;
+    cout<<"Nodos con valor y direccion de otro NODO:"<<endl<<endl;
 
-    cout<<"A.dato = "<<A.dato<<endl
+    cout<<"&A = "<<&A<<endl
+        <<"A.dato = "<<A.dato<<endl
         <<"A.inferior = "<<A.inferior<<endl<<endl;
 
-    cout<<"B.dato = "<<B.dato<<endl
+    cout<<"&B = "<<&B<<endl
+        <<"B.dato = "<<B.dato<<endl
         <<"B.inferior = "<<B.inferior<<endl<<endl;
 
-    cout<<"C.dato = "<<C.dato<<endl
+    cout<<"&C = "<<&C<<endl
+        <<"C.dato = "<<C.dato<<endl
         <<"C.inferior = "<<C.inferior<<endl<<endl;
 
-    cout<<"D.dato = "<<D.dato<<endl
+    cout<<"&D = "<<&D<<endl
+        <<"D.dato = "<<D.dato<<endl
         <<"D.inferior = "<<D.inferior<<endl<<endl;
 
     system("pause");
