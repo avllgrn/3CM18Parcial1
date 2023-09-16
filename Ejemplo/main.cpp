2bda717ec5e08ccbfa604d397cb24f64e584a7e4
Alfredo Villagran Olguin <avillagran@ipn.mx>
2023-09-16 06:36:45 -0600

    Pila de 4 Nodos creada manualmente (sin clase Pila)

--- Ejemplo/main.cpp
+++ Ejemplo/main.cpp
@@ -24,34 +24,55 @@ public:
 int main(void){
     cout<<"Inicio del main"<<endl<<endl;
 
-    Nodo D(3, NULL);    //Se construye Nodo inicializado
-    Nodo C(5, &D);      //Se construye Nodo inicializado
-    Nodo B(7, &C);      //Se construye Nodo inicializado
-    Nodo A(-4, &B);     //Se construye Nodo inicializado
+    Nodo* tope=NULL;            //Pila vacia
+    Nodo* aux;
+
+    tope = new Nodo(3, tope);   //Se construye Nodo dinamicamente (push)
+    tope = new Nodo(5, tope);   //Se construye Nodo dinamicamente (push)
+    tope = new Nodo(7, tope);   //Se construye Nodo dinamicamente (push)
+    tope = new Nodo(-4, tope);  //Se construye Nodo dinamicamente (push)
     system("pause");
     system("cls");
 
     cout<<"Nodos con valor y direccion de otro NODO:"<<endl<<endl;
 
-    cout<<"&A = "<<&A<<endl
-        <<"A.dato = "<<A.dato<<endl
-        <<"A.inferior = "<<A.inferior<<endl<<endl;
+    cout<<"tope = "<<tope<<endl
+        <<"tope->dato = "<<tope->dato<<endl
+        <<"tope->inferior = "<<tope->inferior<<endl;
+    //Se elimina nodo en el tope
+    aux=tope;
+    tope = tope->inferior;
+    delete aux;
+    cout<<endl<<endl;
 
-    cout<<"&B = "<<&B<<endl
-        <<"B.dato = "<<B.dato<<endl
-        <<"B.inferior = "<<B.inferior<<endl<<endl;
+    cout<<"tope = "<<tope<<endl
+        <<"tope->dato = "<<tope->dato<<endl
+        <<"tope->inferior = "<<tope->inferior<<endl;
+    //Se elimina nodo en el tope
+    aux=tope;
+    tope = tope->inferior;
+    delete aux;
+    cout<<endl<<endl;
 
-    cout<<"&C = "<<&C<<endl
-        <<"C.dato = "<<C.dato<<endl
-        <<"C.inferior = "<<C.inferior<<endl<<endl;
+    cout<<"tope = "<<tope<<endl
+        <<"tope->dato = "<<tope->dato<<endl
+        <<"tope->inferior = "<<tope->inferior<<endl;
+    //Se elimina nodo en el tope
+    aux=tope;
+    tope = tope->inferior;
+    delete aux;
+    cout<<endl<<endl;
 
-    cout<<"&D = "<<&D<<endl
-        <<"D.dato = "<<D.dato<<endl
-        <<"D.inferior = "<<D.inferior<<endl<<endl;
+    cout<<"tope = "<<tope<<endl
+        <<"tope->dato = "<<tope->dato<<endl
+        <<"tope->inferior = "<<tope->inferior<<endl;
+    //Se elimina nodo en el tope
+    aux=tope;
+    tope = tope->inferior;
+    delete aux;
+    cout<<endl<<endl;
 
-    system("pause");
-    system("cls");
-    cout<<"Fin del main"<<endl<<endl;//Se destruyen los objetos
+    cout<<"Fin del main"<<endl<<endl;//No quedo ningun nodo en memoria
 
     return 0;
 }
