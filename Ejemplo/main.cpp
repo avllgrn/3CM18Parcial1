5e4f68af3d0f00efd4f422ed6efcba8918b588be
Alfredo Villagran Olguin <avillagran@ipn.mx>
2023-09-29 08:57:57 -0600

    Prueba de clase ColaInt

--- Ejemplo/main.cpp
+++ Ejemplo/main.cpp
@@ -2,112 +2,85 @@
 #include <stdlib.h>
 using namespace std;
 
-class PilaChar{
+class ColaInt{
 private:
     class Nodo{
     public:
-        char dato;
-        Nodo* inferior;
+        int dato;
+        Nodo* detras;
         Nodo(void){
-            dato = 0.0;
-            inferior = NULL;
+            dato = 0;
+            detras = NULL;
             //cout<<"Nodo construido..."<<endl;
         };
-        Nodo(char d, Nodo* i){
-            dato = d;
-            inferior = i;
+        Nodo(int dat, Nodo* det){
+            dato = dat;
+            detras = det;
             //cout<<"Nodo construido..."<<endl;
         };
         ~Nodo(void){
             //cout<<"Nodo destruido..."<<endl;
         };
-    };    Nodo* tope;
+    };
+    Nodo* primero;
+    Nodo* ultimo;
 public:
-    PilaChar(void){
-        tope = NULL;
+    ColaInt(void){
+        primero = NULL;
+        ultimo = NULL;
     };
-    ~PilaChar(void){
-        liberaPila();
+    ~ColaInt(void){
+        liberaCola();
     };
-    void push(char dato){
-        tope = new Nodo(dato, tope);
+    void push(int dato){
+        if(estaVacia()){
+            ultimo = new Nodo(dato,NULL);
+            primero = ultimo;
+        }
+        else{
+            ultimo->detras = new Nodo(dato,NULL);
+            ultimo = ultimo->detras;
+        }
     };
-    char pop(void){
+    int pop(void){
+        int dato;
         Nodo* aux;
-        char d;
-        d = tope->dato;
-        aux = tope;
-        tope = tope->inferior;
-        delete aux;
-        return d;
+        if(primero==ultimo && !estaVacia()){
+            dato = primero->dato;
+            delete primero;
+            primero = NULL;
+            ultimo = NULL;
+        }
+        else{
+            dato = primero->dato;
+            aux = primero;
+            primero = primero->detras;
+            delete aux;
+        }
+        return dato;
     };
     bool estaVacia(void){
-        return tope == NULL;
+        return
+                primero == NULL
+                &&
+                ultimo == NULL
+        ;
     };
-    void liberaPila(void){
+    void liberaCola(void){
         while(!estaVacia()){
-            cout<< pop() << endl;
+            cout<< pop() << " ";
         }
     };
 };
 
-bool esNumerico(char x);
-bool esMayuscula(char x);
-bool esMinuscula(char x);
-bool esLetra(char x);
-bool esCacraterEspecial(char x);
-
 int main(void){
-    string Cadena, Aux;
-    PilaChar P;
-    int i,nCadena,nAux;
-    char c;
+    ColaInt C;
 
-    cout<<"Ingresa tu cadena ";
-    getline(cin,Cadena);
-    nCadena = Cadena.size();
+    C.push(1);
+    C.push(2);
+    C.push(3);
+    C.push(4);
+    C.push(5);
 
-    //Se genera una cadena auxiliar y una pila,
-    for(i=0; i<nCadena; i++){
-        c = Cadena.at(i);//a partir de cada caracter de la original;
-        if(!esCacraterEspecial(c)){//pero, solo con los caracteres no especiales
-            c = toupper(c);//y convertidos a mayusculas
-            Aux.push_back(c);
-            P.push(c);
-        }
-    }
-    nAux = Aux.size();
-
-    //Se comparan todos y cada uno de los caracteres de la cadena auxiliar
-    //con todos y cada uno de los caracteres en la pila,
-    for(i=0; i<nAux; i++)
-        if(Aux[i] != P.pop())//a menos de que se encuentre uno diferente
-            break;
-
-    if(P.estaVacia())
-        cout<<"ES palindromo"<<endl;
-    else
-        cout<<"NO es palindromo"<<endl;
-    cout<<endl<<endl;
-    system("pause");
-    system("cls");
-
-    cout<<"El destructor vacia la pila, dado el caso."<<endl<<endl;
     return 0;
-}
-
-bool esNumerico(char x){
-    return (x>=48 && x<=57);
-}
-bool esMayuscula(char x){
-    return (x>=65 && x<=90);
-}
-bool esMinuscula(char x){
-    return (x>=97 && x<=122);
-}
-bool esLetra(char x){
-    return (esMayuscula(x) || esMinuscula(x));
-}
-bool esCacraterEspecial(char x){
-    return (!esNumerico(x) && !esLetra(x));
 }
