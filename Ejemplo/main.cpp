208a50f3d8e8586ac6e9fa88951839948a321066
Alfredo Villagran Olguin <avillagran@ipn.mx>
2023-09-29 08:51:27 -0600

    Conversion a binario

--- Ejemplo/main.cpp
+++ Ejemplo/main.cpp
@@ -2,40 +2,76 @@
 #include <stdlib.h>
 using namespace std;
 
-bool esNumerico(char x);
-bool esMayuscula(char x);
-bool esMinuscula(char x);
-bool esLetra(char x);
-bool esCacraterEspecial(char x);
-
+class PilaInt{
+private:
+    class Nodo{
+    public:
+        int dato;
+        Nodo* inferior;
+        Nodo(void){
+            dato = 0.0;
+            inferior = NULL;
+            //cout<<"Nodo construido..."<<endl;
+        };
+        Nodo(int d, Nodo* i){
+            dato = d;
+            inferior = i;
+            //cout<<"Nodo construido..."<<endl;
+        };
+        ~Nodo(void){
+            //cout<<"Nodo destruido..."<<endl;
+        };
+    };    Nodo* tope;
+public:
+    PilaInt(void){
+        tope = NULL;
+    };
+    ~PilaInt(void){
+        liberaPila();
+    };
+    void push(int dato){
+        tope = new Nodo(dato, tope);
+    };
+    int pop(void){
+        Nodo* aux;
+        int d;
+        d = tope->dato;
+        aux = tope;
+        tope = tope->inferior;
+        delete aux;
+        return d;
+    };
+    bool estaVacia(void){
+        return tope == NULL;
+    };
+    void liberaPila(void){
+        while(!estaVacia()){
+            cout<< pop() << endl;
+        }
+    };
+};
 
 int main(void){
-    char c;
+    PilaInt P;
+    int n,x;
 
-    cout<<"Ingresa tu caracter ";
-    cin>>c;
+    cout<<"Ingresa n ";
+    cin>>n;
+    x = n;
 
-    cout<<esNumerico(c)<<endl;
-    cout<<esMayuscula(c)<<endl;
-    cout<<esMinuscula(c)<<endl;
-    cout<<esLetra(c)<<endl;
-    cout<<esCacraterEspecial(c)<<endl;
+    while(n>0){//La conversion se almacena en una Pila
+        P.push(n%2);
+        n = n/2;
+    }
+    n = x;
+
+    cout<<endl<<endl
+        <<n<<" = ";
+
+    while(!P.estaVacia())//La conversion se muestra, vaciando la Pila
+        cout<<P.pop();
+
+    cout<<endl<<endl;
 
     return 0;
-}
-
-bool esNumerico(char x){
-    return (x>=48 && x<=57);
-}
-bool esMayuscula(char x){
-    return (x>=65 && x<=90);
-}
-bool esMinuscula(char x){
-    return (x>=97 && x<=122);
-}
-bool esLetra(char x){
-    return (esMayuscula(x) || esMinuscula(x));
-}
-bool esCacraterEspecial(char x){
-    return (!esNumerico(x) && !esLetra(x));
 }
