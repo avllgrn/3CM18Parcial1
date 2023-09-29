b21ee49f55b00b05cffb39da5b82a957a50ea960
Alfredo Villagran Olguin <avillagran@ipn.mx>
2023-09-29 08:57:34 -0600

    Palindromo con dos cadenas y una pila

--- Ejemplo/main.cpp
+++ Ejemplo/main.cpp
@@ -51,33 +51,63 @@ public:
     };
 };
 
-int main(void){
-    string Cadena;
-    PilaChar P;
-    int i,nCadena;
+bool esNumerico(char x);
+bool esMayuscula(char x);
+bool esMinuscula(char x);
+bool esLetra(char x);
+bool esCacraterEspecial(char x);
 
-    cout<<"Ingresa tu numero ";
+int main(void){
+    string Cadena, Aux;
+    PilaChar P;
+    int i,nCadena,nAux;
+    char c;
+
+    cout<<"Ingresa tu cadena ";
     getline(cin,Cadena);
     nCadena = Cadena.size();
 
-    //Se genera una pila,
-    for(i=0; i<nCadena; i++)
-        P.push(Cadena.at(i));//a partir de cada caracter numerico de la original;
+    //Se genera una cadena auxiliar y una pila,
+    for(i=0; i<nCadena; i++){
+        c = Cadena.at(i);//a partir de cada caracter de la original;
+        if(!esCacraterEspecial(c)){//pero, solo con los caracteres no especiales
+            c = toupper(c);//y convertidos a mayusculas
+            Aux.push_back(c);
+            P.push(c);
+        }
+    }
+    nAux = Aux.size();
 
-    //Se comparan todos y cada uno de los caracteres de la cadena
+    //Se comparan todos y cada uno de los caracteres de la cadena auxiliar
     //con todos y cada uno de los caracteres en la pila,
-    for(i=0; i<nCadena; i++)
-        if(Cadena[i] != P.pop())//a menos de que se encuentre uno diferente
+    for(i=0; i<nAux; i++)
+        if(Aux[i] != P.pop())//a menos de que se encuentre uno diferente
             break;
 
     if(P.estaVacia())
-        cout<<"ES capicua"<<endl;
+        cout<<"ES palindromo"<<endl;
     else
-        cout<<"NO es capicua"<<endl;
+        cout<<"NO es palindromo"<<endl;
     cout<<endl<<endl;
     system("pause");
     system("cls");
-    cout<<"El destructor vacia la pila, dado el caso."<<endl<<endl;
 
+    cout<<"El destructor vacia la pila, dado el caso."<<endl<<endl;
     return 0;
+}
+
+bool esNumerico(char x){
+    return (x>=48 && x<=57);
+}
+bool esMayuscula(char x){
+    return (x>=65 && x<=90);
+}
+bool esMinuscula(char x){
+    return (x>=97 && x<=122);
+}
+bool esLetra(char x){
+    return (esMayuscula(x) || esMinuscula(x));
+}
+bool esCacraterEspecial(char x){
+    return (!esNumerico(x) && !esLetra(x));
 }
